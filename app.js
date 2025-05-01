a86160c90342a4c92c3a26847d98d912caf0c9b3
Rajiv Ranjan <coolmonk@Rajivs-MacBook-Air-2.local>
2025-05-01 19:58:13 +0530

    cors added

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const indexRoutes = require("./routes");
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/", indexRoutes);
 
@@ -18,6 +20,6 @@ mongoose
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-app.listen(PORT, () => {
+app.listen(PORT, "127.0.0.1", () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
 });
